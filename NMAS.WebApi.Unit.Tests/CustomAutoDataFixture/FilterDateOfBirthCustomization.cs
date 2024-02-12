28bf7a6c38bce458a381dbb1ca7d65a990e1b503
nedas127 <kontenis.ned@gmail.com>
2024-02-12 21:14:24 +0200

    Adding tests for list endpoint

---
+++ NMAS.WebApi.Unit.Tests/CustomAutoDataFixture/FilterDateOfBirthCustomization.cs
@@ -0,0 +1,16 @@
+﻿using AutoFixture;
+using NMAS.WebApi.Contracts.IllegalMigrantEntity;
+using System;
+
+namespace NMAS.WebApi.Unit.Tests.CustomAutoDataFixture
+{
+    public class FilterDateOfBirthCustomization : ICustomization
+    {
+        public void Customize(IFixture fixture)
+        {
+            fixture.Customize<FilterDateOfBirth>(composer => composer
+                .With(x => x.From, DateTime.Now.AddYears(-10))
+                .With(x => x.To, DateTime.Now));
+        }
+    }
+}
