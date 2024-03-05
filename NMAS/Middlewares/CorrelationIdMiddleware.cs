f0d2538bbefd53eeb8a21d27177d7a1cf39d3254
Nedas <nedas.kontenis@stud.vilniustech.lt>
2024-03-05 13:53:53 +0200

    https://nmas-programu-kurimo-procesas.atlassian.net/browse/NMAS-13

---
+++ NMAS/Middlewares/CorrelationIdMiddleware.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+using System;
+
+namespace NMAS.WebApi.Host.Middlewares
+{
+    public class CorrelationIdMiddleware
+    {
+        private const string CorrelationIdHeaderKey = "X-Correlation-ID";
+        private readonly RequestDelegate _next;
+
+        public CorrelationIdMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (!context.Request.Headers.TryGetValue(CorrelationIdHeaderKey, out var correlationId))
+            {
+                correlationId = Guid.NewGuid().ToString();
+                context.Request.Headers.Append(CorrelationIdHeaderKey, correlationId);
+            }
+
+            context.Response.OnStarting(() => {
+                context.Response.Headers.Append(CorrelationIdHeaderKey, new[] { correlationId.ToString() });
+                return Task.CompletedTask;
+            });
+
+            await _next(context);
+        }
+    }
+}
